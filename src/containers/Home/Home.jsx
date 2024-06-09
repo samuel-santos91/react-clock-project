70632536f254874d0583ab1b0341a86abfa38bd2
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2024-06-10 04:45:09 +1000

    adds rensposiveness to design

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -3,9 +3,11 @@ import React from "react";
 import AnalogClock from "../AnalogClock/AnalogClock";
 import DigitalClock from "../DigitalClock/DigitalClock";
 
+import classes from './Home.module.scss';
+
 const Home = () => {
   return (
-    <main>
+    <main className={classes.display}>
       <AnalogClock />
       <DigitalClock />
     </main>
