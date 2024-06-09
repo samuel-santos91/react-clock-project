5803bfc8b16f972cb22a50c8a9375f38a2663efd
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2024-06-10 04:28:02 +1000

    styles digital clock

--- src/containers/DigitalClock/DigitalClock.jsx
+++ src/containers/DigitalClock/DigitalClock.jsx
@@ -2,12 +2,14 @@ import React, { useContext } from "react";
 
 import { TimeContext } from "../../context/TimeContextProvider";
 
+import classes from './DigitalClock.module.scss';
+
 const DigitalClock = () => {
   const { formattedTime } = useContext(TimeContext);
 
   return (
     <section>
-      <time>
+      <time className={classes.digital}>
         <span>{formattedTime?.hours}</span>
         <span>:</span>
         <span>{formattedTime?.minutes}</span>
